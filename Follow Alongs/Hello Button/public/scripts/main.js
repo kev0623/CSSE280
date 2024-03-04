0cd2c47df8120051d41b29ad99747eee87ca246e
kev0623 <link4@rose-hulman.edu>
2024-03-04 00:35:33 -0500

    javascript interaction

--- Follow Alongs/Hello Button/public/scripts/main.js
+++ Follow Alongs/Hello Button/public/scripts/main.js
@@ -1,7 +1,29 @@
+var counter = 0;
+
+
 function main(){
     console.log("Ready");
+    document.querySelector("#decButton").onclick =(event) => {
+        console.log("Decrement");
+        counter = counter -1;
+        updateView();
+    }
+    document.querySelector("#resetButton").onclick =(event) => {
+        console.log("Reset");
+        counter = 0;
+        updateView();
+    }
+    document.querySelector("#incButton").onclick =(event) => {
+        console.log("Increment");
+        counter = counter +1;
+        updateView();
+    }
 }
 
+function updateView(){
+    // document.querySelector("#countText").innerHTML = "Count = "+counter;
+    document.querySelector("#countText").innerHTML = `Count = ${counter}`;
 
+}
 
 main();
