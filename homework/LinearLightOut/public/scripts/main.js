dc3921c24275a8d14d74d6475b0e648b1a6c8c82
kev0623 <link4@rose-hulman.edu>
2024-04-01 08:23:53 -0400

    Can not add new quote

--- homework/LinearLightOut/public/scripts/main.js
+++ homework/LinearLightOut/public/scripts/main.js
@@ -18,7 +18,7 @@ rh.functionName = function () {
 
 rh.Game = class {
 
-	static NUM_BUTTONS = 9;
+	static NUM_BUTTONS = 7;
 	static Mark = {
 		1: "1",
 		0: "0",
@@ -27,11 +27,12 @@ rh.Game = class {
 	static State = {
 		START: "Make the buttons match",
 		PLAYING: "You have taken 1 move so far",
-		WIN: "You win in 5 moves"
+		WIN: "You win in  move"
 	}
 	/** constructor */
 	constructor() {
 		this.board = [];
+		this.count = 0;
 		for (let k = 0; k < rh.Game.NUM_BUTTONS; k++) {
 			var temp = Math.random();
 			if (temp < 0.5) {
@@ -42,13 +43,16 @@ rh.Game = class {
 
 		}
 		this.State = rh.Game.State.START;
-	
+		document.querySelector("#gameStateText").textContent = `Make the buttons match`;
 	}
 
 	pressedButtonAtIndex(buttonIndex) {
 		// Controller is telling the Model to make a change
-		
-		
+		if (this.isGameWon()) {
+			return;
+		}
+		this.count++;
+		document.querySelector("#gameStateText").textContent = `You have taken ${this.count} moves so far`;
 		if(buttonIndex==0){
 			if(this.board[0]==0){
 				this.board[0]=1;
@@ -80,20 +84,22 @@ rh.Game = class {
 				this.board[buttonIndex-1]=0;
 			}
 		}
-		// Check for a win
-		const linesOf3 = []; 
-		linesOf3.push(this.board[0] + this.board[1] + this.board[2] + this.board[3]
-			 + this.board[4] + this.board[5] + this.board[6]);
-		for (const line of linesOf3) {
-			if (line == "000000" ||line =="111111") {
-				console.log("hello");
-				document.querySelector("#gameStateText").innerHTML = "Win the Game";
-				this.state = rh.Game.State.WIN;
-			} 
-		}
-		
+     if(this.isGameWon()==true){
+		document.querySelector("#gameStateText").textContent = `You win in ${this.count} moves`;
+		 this.State = rh.Game.State.WIN;
+	 }
 	}
-
+	isGameWon() {
+		// Check for a win
+		console.log(this.board);
+		let base = this.board[0]
+		for (let i = 1; i < this.board.length; i++) {
+			if (base != this.board[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 	getMarkAtIndex(buttonIndex) {
 		// Returns "1", "0".
 		return this.board[buttonIndex];
@@ -112,12 +118,42 @@ rh.PageController = class {
 			this.game.pressedButtonAtIndex(buttonId);
 			this.updateView();
 		});
+		document.querySelector("#newGameButton").onclick = (event) => {
+			this.game = new rh.Game();
+			document.querySelector("#gameStateText").textContent = `Make the buttons match`;
+			this.updateView();
+		}
+		// $("#newGameButton").click((event) => {
+		// 	for (let k = 0; k < rh.Game.NUM_BUTTONS; k++) {
+		// 		var temp = Math.random();
+		// 		if (temp < 0.5) {
+		// 			this.board[k] = 1;
+		// 		} else {
+		// 			this.board[k] = 0;
+		// 		}
+	
+		// 	}
+		// 	this.updateView();
+		// });
 	}
 	updateView() {
-		$("#gameStateText").html("Make the buttons match");
+		if(this.game.State== "WIN"){
+			document.querySelector("#gameStateText").textContent = `You win in ${this.count} moves`;
+		}
+		if(this.game.State== "START"){
+			document.querySelector("#gameStateText").textContent = `Make the buttons match`;
+		}
+
 		$("#gameBoard>button").each((index, item) => {
 			$(item).html(this.game.getMarkAtIndex(index));
 		});
+		for(var i=0;i<7;i++){
+			if(this.game.getMarkAtIndex(i)==1){
+				$("#gameBoard>button").eq(i).css("background-color","orange");
+			}else{
+				$("#gameBoard>button").eq(i).css("background-color","grey");
+			}
+		} 
 	}
 
 
