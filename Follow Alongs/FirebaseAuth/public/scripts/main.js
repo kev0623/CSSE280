2b6e7c687ba373ab7f89138957f8ef3b29f76c4b
kev0623 <link4@rose-hulman.edu>
2024-04-09 00:45:20 -0400

    email password

--- Follow Alongs/FirebaseAuth/public/scripts/main.js
+++ Follow Alongs/FirebaseAuth/public/scripts/main.js
@@ -1,36 +1,92 @@
 /**
  * @fileoverview
- * Provides the JavaScript interactions for all pages.
+ * Provides interactions for all pages in the UI.
  *
- * @author 
- * PUT_YOUR_NAME_HERE
+ * @author  Dave Fisher
  */
 
 /** namespace. */
-var rhit = rhit || {};
+var rh = rh || {};
 
-/** globals */
-rhit.variableName = "";
+rh.beginAuthListening = function () {
+	firebase.auth().onAuthStateChanged(function (user) {
+		if (user) {
+			// User is signed in.
+			$("#uid").html(`<b>uid</b>: ${user.uid}`);
+			$("#email").html(`<b>email</b>: ${user.email}`);
+			$("#displayName").html(`<b>displayName</b>: ${user.displayName}`);
+			$("#photoURL").attr("src", user.photoURL);
+			$("#phoneNumber").html(`<b>phone #</b>: ${user.phoneNumber}`);
+			console.log(user.providerData);
+			console.log("A user IS signed in.  Uid = ", user.uid);
+			
+			$("#firebaseui-auth-container").hide();
+			$("#emailPassword").hide();
+			$("#userInfo").show();
+		} else {
+			// User is signed out.
+			console.log("There is no user.  Nobody is signed in.");
+			$("#emailPassword").hide();  // Turned off for now.
+			$("#firebaseui-auth-container").show();
+			$("#userInfo").hide();
+		}
+	});
+}
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
+rh.enableEmailPassword = function () {
+	const email = new mdc.textField.MDCTextField(document.querySelector('.email'));
+	const password = new mdc.textField.MDCTextField(document.querySelector('.password'));
+	new mdc.ripple.MDCRipple(document.querySelector('#createAccount'));
+	new mdc.ripple.MDCRipple(document.querySelector('#login'));
+
+	$("#createAccount").click((event) => {
+		const emailValue = $("#email-input").val();
+		const passwordValue = $("#password-input").val();
+		console.log("Create a new user", emailValue, passwordValue);
+		firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue).catch(function (error) {
+			// CONSIDER: In a real tell the user what is wrong.
+			console.log(`Error ${error.code}: ${error.message}`);
+		});
+	});
+	$("#login").click((event) => {
+		console.log("Log in an existing user");
+		const emailValue = $("#email-input").val();
+		const passwordValue = $("#password-input").val();
+		console.log("Create a new user", emailValue, passwordValue);
+		firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue).catch(function (error) {
+			// CONSIDER: In a real tell the user what is wrong.
+			console.log(`Error ${error.code}: ${error.message}`);
+		});
+	});
 };
 
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
+rh.startFirebaseUi = function () {
+	// FirebaseUI config.
+	var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+          firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+        ],
+      };
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start("#firebaseui-auth-container", uiConfig);
 }
 
 /* Main */
-/** function and class syntax examples */
-rhit.main = function () {
+$(document).ready(() => {
 	console.log("Ready");
-};
+	// Kept for reference only
+	// const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+	// const topAppBar = new mdc.topAppBar.MDCTopAppBar(topAppBarElement);
 
-rhit.main();
+	rh.enableEmailPassword();
+	rh.beginAuthListening();
+	rh.startFirebaseUi();
+
+	$("#signOut").click((event) => {
+		firebase.auth().signOut()
+	});
+});
