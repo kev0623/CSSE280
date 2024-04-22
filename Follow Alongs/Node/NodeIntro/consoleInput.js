220bcdcc1973c06d37279378fd5b742b2dacb8c8
Kai-Chun Lin <kevin6230k6@gmail.com>
2024-04-22 17:33:20 -0400

    add noExpress Node.js

--- Follow Alongs/Node/NodeIntro/consoleInput.js
+++ Follow Alongs/Node/NodeIntro/consoleInput.js
@@ -1,6 +1,8 @@
+console.clear();
 process.stdout.write("What is your name? ");
 
 process.stdin.on("data", (data) =>{
+    console.log("data", data)
     process.stdout.write("Hello"+data.toString().trim()+"\n");
     process.exit();
 });
--- Follow Alongs/Node/NodeIntro/npmAsciiFun.js
+++ Follow Alongs/Node/NodeIntro/npmAsciiFun.js
@@ -1,11 +1,19 @@
 console.log("TODO: learn to use npm!");
 var figlet = require("figlet");
 
-figlet("I love you !!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+// figlet("I love you !!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+const imgToAscii = require('ascii-img-canvas-nodejs')
+
+const opts = {}
+
+const asciiImgLocal = imgToAscii('files/885px-Node.js_logo.svg.png', opts);
+asciiImgLocal.then(ascii => console.log(ascii))
+console.log(asciiImgLocal)
+
