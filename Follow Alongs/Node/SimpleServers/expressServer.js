9a5d89cbb2836b0de1f202ad1e572ee7eef1135a
Kai-Chun Lin <kevin6230k6@gmail.com>
2024-04-23 10:08:40 -0400

    Hello button render with pug

--- Follow Alongs/Node/SimpleServers/expressServer.js
+++ Follow Alongs/Node/SimpleServers/expressServer.js
@@ -1,10 +1,40 @@
-var express = require("express");
-var app = express();
+    var express = require("express");
+    var app = express();
+    let counter = 0;
+    app.use('/static',express.static("public"));
 
-app.use('/static',express.static("public"));
+    app.post("/myPost", function(req,res){
+        res.send("HTML cod. Done via a post request");
+    })
+    app.get("/users/:username", function(req,res){
+        let username = req.params.username;
+        res.send("<h1>"+ username+"</h1>");
+    })
+
+    app.get("/hello",function (req,res){
+        let name = req.query.name;
+        let age = req.query.age;
 
 
-app.get("/hello",function (req,res){
-    res.send("<h1>Hello World!</h1>")
-});
-app.listen(3000);
+
+        res.send("<h1>Hello " + name + "!</h1>"
+            +"You are "+ age + "years old."
+        );
+    });
+
+
+    app.set('views','./views')
+    app.set('view engine','pug')
+
+    app.get('/pug/', function(req,res){
+        let array = [
+            {name: "Kai"},
+            {name: "Quang"},
+            {name: "Anh"}
+        ]
+        res.render('index',{title: 'Hey', message: 'Hello there!',arr: array})
+    })
+    app.get('/pug/hello', function(req,res){
+        res.render('hello',{ title: 'Hello Button',count: counter})
+    })
+    app.listen(3000);
