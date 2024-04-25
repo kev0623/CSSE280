7e38955220dd0c287b535a2cee2a12bbb6b77d93
Kai-Chun Lin <143916746+kev0623@users.noreply.github.com>
2024-04-25 11:11:58 -0400

    Not yet implement list page

--- template-master/public/scripts/main.js
+++ template-master/public/scripts/main.js
@@ -1,37 +1,36 @@
-function decodeMorse(message) {
-    let alphabet = {
-        "-----":"0", ".----":"1", "..---":"2", "...--":"3", "....-":"4",
-        ".....":"5", "-....":"6", "--...":"7", "---..":"8", "----.":"9",
-        ".-":"a", "-...":"b", "-.-.":"c", "-..":"d", ".":"e", "..-.":"f",
-        "--.":"g", "....":"h", "..":"i", ".---":"j", "-.-":"k", ".-..":"l",
-        "--":"m", "-.":"n", "---":"o", ".--.":"p", "--.-":"q", ".-.":"r",
-        "...":"s", "-":"t", "..-":"u", "...-":"v", ".--":"w", "-..-":"x",
-        "-.--":"y", "--..":"z", "/":" ", "-.-.--":"!", ".-.-.-":".", "--..--":","
-    };
-    return message.split("//").map(word => 
-        word.split("/").map(letter => alphabet[letter]).join("")
-    ).join(" ");
+/**
+ * @fileoverview
+ * Provides the JavaScript interactions for all pages.
+ *
+ * @author 
+ * PUT_YOUR_NAME_HERE
+ */
+
+/** namespace. */
+var rhit = rhit || {};
+
+/** globals */
+rhit.variableName = "";
+
+/** function and class syntax examples */
+rhit.functionName = function () {
+	/** function body */
+};
+
+rhit.ClassName = class {
+	constructor() {
+
+	}
+
+	methodName() {
+
+	}
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const transmissionElement = document.getElementById('transmission');
-    const translationElement = document.getElementById('translation');
-    
-    document.querySelectorAll('button').forEach(button => {
-        button.addEventListener('click', () => {
-            const char = button.getAttribute('data-transmits');
-            let currentTransmission = transmissionElement.innerText === "Transmission will go here" ? "" : transmissionElement.innerText;
-            if (char === 'dot') {
-                currentTransmission += '.';
-            } else if (char === 'dash') {
-                currentTransmission += '-';
-            } else if (char === 'letterspace') {
-                currentTransmission += '/';
-            } else if (char === 'wordspace') {
-                currentTransmission += '//';
-            }
-            transmissionElement.innerText = currentTransmission;
-            translationElement.innerText =  decodeMorse(currentTransmission);
-        });
-    });
-});
+/* Main */
+/** function and class syntax examples */
+rhit.main = function () {
+	console.log("Ready");
+};
+
+rhit.main();
