47081970cfe79e0e91fe75d416f6d3f16a4358ab
Kai-Chun Lin <143916746+kev0623@users.noreply.github.com>
2024-04-26 00:47:40 -0400

    Node server API completed

--- Follow Alongs/Node/TTTNode/app.js
+++ Follow Alongs/Node/TTTNode/app.js
@@ -2,4 +2,30 @@ const express = require("express");
 
 var app = express();
 app.use(express.static("public"));
+
+
+
+app.get("/api/getmove/:board", (request, response) => {
+    const boardString = request.params.board;
+
+    const openLocations = getOpenLocations(boardString);
+
+    const moveSelected = openLocations[Math.floor(Math.random() * openLocations.length)]
+    console.log(boardString);
+    response.send({"move": moveSelected});
+});
+
+function getOpenLocations(boardString) {
+    const openLocations = [];
+    for (var i = 0; i < boardString.length; i++) {
+        if (boardString.charAt(i) == '-') {
+            openLocations.push(i)
+        }
+    }
+    return openLocations;
+}
+
+console.log(getOpenLocations("-OO-OOOOO"));
+
+
 app.listen(3000);
